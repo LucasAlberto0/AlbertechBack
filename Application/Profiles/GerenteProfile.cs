7a6bdd6c09abfd6ef39cdd49ad28688e8e60a242
Lucas Alberto <lucasalberto4321@gmail.com>
2025-11-05 15:49:44 -0300

    fix: criacao-de-conta-gerente-com-email-duplicado

--- Application/Profiles/GerenteProfile.cs
+++ Application/Profiles/GerenteProfile.cs
@@ -5,6 +5,7 @@ public class GerenteProfile : Profile
     public GerenteProfile()
     {
         CreateMap<CadastroGerenteDto, GerenteModel>()
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email));
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
     }
 }
